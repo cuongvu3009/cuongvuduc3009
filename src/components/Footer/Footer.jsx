ece95ad06ef3a85eb4c1a704e42ed6b5d32dce75
cuongvu3009 <cuongvu3009@gmail.com>
2022-02-28 23:56:23 +0200

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import './footer.css';
+const Footer = () => (
+  <div className='footerContainer'>
+    <span>© {new Date().getFullYear()} Cuong Vu Portfolio</span>
+  </div>
+);
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Intro from '../components/Intro/Intro';
 import About from '../components/About/About.jsx';
 import ProjectList from '../components/ProJectList/ProjectList';
 import Contact from '../components/Contact/Contact';
+import Footer from '../components/Footer/Footer';
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <About />
       <ProjectList />
       <Contact />
+      <Footer />
     </>
   );
 };
